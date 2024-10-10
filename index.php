654d2531311c8ce5285ad5f94e69e723a65d4588
DylanBlanco <dylanblanco1198@gmail.com>
2024-10-10 22:22:40 +0200

    definito metodo setTitle()

--- index.php
+++ index.php
@@ -7,11 +7,25 @@
         public $year;
 
         // costruttore
-        function __construct($title, $genres, $year, $casting) {
-            $this -> title = $title;
+        function __construct(
+            string $title, 
+            string $genres, 
+            int $year, 
+            array $casting) {
+            $this -> setTitle($title);
             $this -> genres = $genres;
             $this -> casting = $casting;
             $this -> year = $year;
+        }
+
+        // function
+        public function setTitle() {
+            if (is_string($title)) {
+                $this -> title = $title;
+            }
+            else {
+                $this -> title = 'non valido';
+            }
         }
     }
 
@@ -32,49 +46,33 @@
             'Leonardo DiCaprio',
             'Ben Kingsley',
         ];
-        
+
     $film = new Movie('Shutter Island','Thriller', 2010, $casting);
     var_dump($film);
+    var_dump($film -> setTitle());
+    var_dump($film -> setTitle());
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>PHP OOP 1</title>
 
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-        
         <header class="py-4 mb-4">
             <div class="container">
                 <div class="row">
                     <div class="col text-center">
                         <h1>
-                            Template PHP
+                        PHP OOP 1
                         </h1>
                     </div>
                 </div>
             </div>
         </header>
-
-        <main>
-            <div class="container">
-                <div class="row">
-                    <?php
-                        for ($i = 0; $i < $columnsNumber; $i++) {
-                    ?>
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                            Colonna <?php echo $i + 1; ?>
-                        </div>
-                    <?php
-                        }
-                    ?>
-                </div>
-            </div>
-        </main>
-
     </body>
 </html>
